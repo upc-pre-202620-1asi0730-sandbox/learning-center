62c5b6e802e419ef6f59307125ea07ab2e075ac2
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-21 11:59:43 -0500

    feat(ui): add About view with i18n support.

---
+++ src/shared/presentation/views/about.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {useI18n} from "vue-i18n";
+
+const { t } = useI18n();
+</script>
+
+<template>
+  <div class="align-content-start justify-content-start m-4">
+    <h1>{{ t('about.title')}}</h1>
+    <img src="/acme-logo.svg" alt="ACME Logo" width="200" height="200"/>
+    <p>{{ t('about.content') }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
