6581a79d57f60ee6f63dab73022047af2e348680
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-21 08:33:20 -0500

    feat(i18n): add vue-i18n setup with basic locale files.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './app.vue'
+import i18n from "./i18n.js";
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(i18n)
+    .mount('#app')
