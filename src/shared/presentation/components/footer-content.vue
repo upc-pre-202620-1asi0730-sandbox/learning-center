c3a17df43ca2543e89c498f9dbda4dfe4732da1d
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-21 11:59:57 -0500

    feat(ui): add footer content component with i18n integration.

---
+++ src/shared/presentation/components/footer-content.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="grid bg-primary mt-4 p-2 align-content-start">
+    <div class="col-12 ml-3 align-items-center justify-content-center">
+      <p>Copyright &copy; 2025. ACME Studios</p>
+    </div>
+    <div  class="col-12 ml-3 mt-1 align-items-center justify-content-center">
+      <p>
+        {{ $t('authoring-phrase.intro') }} <i class="pi pi-heart"/>
+        {{ $t('authoring-phrase.use') }} <a href="https://primevue.org/" target="_blank">PrimeVue</a>
+        {{ $t('authoring-phrase.author', {brand: 'ACME'}) }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
