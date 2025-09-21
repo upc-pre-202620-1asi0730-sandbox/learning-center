507e3a0ec7770e54e6020ed1fec35106dccddebd
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-21 12:00:53 -0500

    feat(ui): add home view with i18n support.

---
+++ src/shared/presentation/views/home.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {useI18n} from "vue-i18n";
+const { t } = useI18n();
+
+</script>
+
+<template>
+  <div class="align-content-start justify-content-start m-4">
+    <h1>{{  t('home.title') }}</h1>
+    <p>{{ t('home.content')}}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
