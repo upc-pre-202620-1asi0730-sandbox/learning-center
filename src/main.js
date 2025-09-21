b08bdf64be462810937e3edc1f7c9677e86dcf13
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-21 09:46:31 -0500

    feat(ui): integrate primevue and theme setup.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,71 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './app.vue'
 import i18n from "./i18n.js";
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import Material from '@primeuix/themes/material';
+import PrimeVue from 'primevue/config';
+import {
+    Button,
+    Card,
+    Checkbox,
+    Column,
+    ConfirmationService,
+    ConfirmDialog,
+    DataTable,
+    Dialog,
+    DialogService,
+    Drawer,
+    FileUpload,
+    FloatLabel,
+    IconField,
+    InputIcon,
+    InputNumber,
+    InputText,
+    Menu,
+    Rating,
+    Row,
+    Select,
+    SelectButton,
+    Tag,
+    Textarea,
+    Toast,
+    ToastService,
+    Toolbar,
+    Tooltip
+} from "primevue";
+import router from "./router.js";
 
+// noinspection JSCheckFunctionSignatures
 createApp(App)
     .use(i18n)
+    .use(PrimeVue, {theme: { preset: Material}, ripple: true})
+    .use(ConfirmationService)
+    .use(DialogService)
+    .use(ToastService)
+    .component('pv-button',         Button)
+    .component('pv-card',           Card)
+    .component('pv-column',         Column)
+    .component('pv-confirm-dialog', ConfirmDialog)
+    .component('pv-checkbox',       Checkbox)
+    .component('pv-data-table',     DataTable)
+    .component('pv-dialog',         Dialog)
+    .component('pv-select',         Select)
+    .component('pv-select-button',  SelectButton)
+    .component('pv-file-upload',    FileUpload)
+    .component('pv-float-label',    FloatLabel)
+    .component('pv-icon-field',     IconField)
+    .component('pv-input-icon',     InputIcon)
+    .component('pv-input-text',     InputText)
+    .component('pv-input-number',   InputNumber)
+    .component('pv-menu',           Menu)
+    .component('pv-rating',         Rating)
+    .component('pv-row',            Row)
+    .component('pv-drawer',         Drawer)
+    .component('pv-tag',            Tag)
+    .component('pv-textarea',       Textarea)
+    .component('pv-toolbar',        Toolbar)
+    .component('pv-toast',          Toast)
+    .directive('tooltip',           Tooltip)
+    .use(router)
     .mount('#app')
