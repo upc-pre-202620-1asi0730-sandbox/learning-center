7e2c70ef8d1a98dd751f7dbc760da28fa35787b1
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-29 00:14:47 -0500

    refactor(publishing): simplify `category` entity by removing private fields and accessor methods.

--- src/publishing/domain/model/category.entity.js
+++ src/publishing/domain/model/category.entity.js
@@ -4,48 +4,14 @@
  */
 export class Category {
     /**
-     * @type {?number}
-     * @private
-     */
-    #id;
-    /**
-     * @type {string}
-     * @private
-     */
-    #name;
-
-    /**
      * Creates a new Category instance.
      * @param {Object} params - The parameters for the category.
      * @param {?number} [params.id=null] - The unique identifier for the category.
      * @param {string} [params.name=''] - The name of the category.
      */
     constructor({ id = null, name = ''}) {
-        this.#id = id;
-        this.#name = name;
+        this.id = id;
+        this.name = name;
     }
 
-    /**
-     * Gets the category ID.
-     * @returns {?number} The unique identifier for the category.
-     */
-    get id() {
-        return this.#id;
-    }
-
-    /**
-     * Gets the category name.
-     * @returns {string} The name of the category.
-     */
-    get name() {
-        return this.#name;
-    }
-
-    /**
-     * Sets the category name.
-     * @param {string} name - The new name for the category.
-     */
-    set name(name) {
-        this.#name = name;
-    }
 }
