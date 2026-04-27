58dfc42a62594908362e2f2eb042d73f7da8be4c
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-26 22:07:27 -0500

    feat(router): update routes for iam implementation and adjust api url for development.

--- src/router.js
+++ src/router.js
@@ -1,16 +1,29 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Home from "./shared/presentation/views/home.vue";
 import publishingRoutes from "./publishing/presentation/publishing-routes.js";
-import iamRoutes from "./iam/presentation/iam-routes.js";
+// To import when IAM is implemented
+// import iamRoutes from "./iam/presentation/iam-routes.js";
 
-// TODO: Define lazy-loaded components for routes
+// Define lazy-loaded components for routes
 const about = () => import('./shared/presentation/views/about.vue');
 const pageNotFound = () => import('./shared/presentation/views/page-not-found.vue');
+/*
+// Routes version when IAM is implemented
 const routes = [
     { path: '/home',            name: 'home',       component: Home,        meta: { title: 'Home' } },
     { path: '/about',           name: 'about',      component: about,       meta: { title: 'About' } },
     { path: '/publishing',      name: 'publishing', children: publishingRoutes },
     { path: '/iam',             name: 'iam',        children: iamRoutes },
+    { path: '/',                redirect: '/home' },
+    { path: '/:pathMatch(.*)*', name: 'not-found', component: pageNotFound, meta: { title: 'Page Not Found' } }
+];
+*/
+
+// Routes version when IAM is not implemented
+const routes = [
+    { path: '/home',            name: 'home',       component: Home,        meta: { title: 'Home' } },
+    { path: '/about',           name: 'about',      component: about,       meta: { title: 'About' } },
+    { path: '/publishing',      name: 'publishing', children: publishingRoutes },
     { path: '/',                redirect: '/home' },
     { path: '/:pathMatch(.*)*', name: 'not-found', component: pageNotFound, meta: { title: 'Page Not Found' } }
 ];
@@ -35,8 +48,10 @@ router.beforeEach((to, from, next) => {
     // Set the page title
     let baseTitle = 'ACME Learning Center';
     document.title = `${baseTitle} - ${to.meta['title']}`;
-    // TODO: Call authentication guard
-    next();
+    // When IAM is implemented use:
+    // return authenticationGuard(to, from, next);
+    // if not, use:
+    return next();
 });
 
 export default router;
