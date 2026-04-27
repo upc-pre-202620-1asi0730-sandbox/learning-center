3f5b29b8b20bd83b099bd937bf79bf3c425b2426
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-26 22:07:47 -0500

    feat(i18n): enhance locale display in language switcher component.

--- src/shared/presentation/components/language-switcher.vue
+++ src/shared/presentation/components/language-switcher.vue
@@ -1,11 +1,15 @@
 <script setup>
   import {useI18n} from "vue-i18n";
-  const { t, locale, availableLocales } = useI18n();
+  const { locale, availableLocales } = useI18n();
 
 </script>
 
 <template>
-  <pv-select-button v-model="locale" :options="availableLocales"/>
+  <pv-select-button v-model="locale" :options="availableLocales">
+    <template #option="slotProps">
+      <span>{{ slotProps.option.toUpperCase() }}</span>
+    </template>
+  </pv-select-button>
 </template>
 
 <style scoped>
